bb93738e9d1acf7b93414154e5e0137e695200e0
Олександр Пилявський <vt231_poi@student.ztu.edu.ua>
2024-04-20 17:35:59 +0300

    Реалізований рух

--- MainMenu/GameForm.xaml.cs
+++ MainMenu/GameForm.xaml.cs
@@ -11,7 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Navigation;
 using MainMenu.Pages;
+using System.Windows.Threading;
 
 namespace MainMenu
 {
@@ -21,16 +23,90 @@ namespace MainMenu
     public partial class GameForm : Window
     {
         EscMenu Menu = new EscMenu();
+        private DispatcherTimer GameTimer = new DispatcherTimer();
+        
+        private bool UpKeyPressed, DownKeyPressed, LeftKeyPressed, RightKeyPressed;
+        private float SpeedX, SpeedY, Friction = 0.75f, Speed = 2;
         public GameForm()
         {
             InitializeComponent();
+            GameScreen.Focus();
+
+            GameTimer.Interval = TimeSpan.FromMilliseconds(16);
+            GameTimer.Tick += GameTick;
+            GameTimer.Start();
+        }
+        private void KeyBoardUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.W)
+            {
+                UpKeyPressed = false;
+            }
+            if (e.Key == Key.A)
+            {
+                LeftKeyPressed = false;
+            }
+            if (e.Key == Key.S)
+            {
+                DownKeyPressed = false;
+            }
+            if (e.Key == Key.D)
+            {
+                RightKeyPressed = false;
+            }
+        }
+        private void KeyBoardDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.W)
+            {
+                UpKeyPressed = true;
+            }
+            if (e.Key == Key.A)
+            {
+                LeftKeyPressed = true;
+            }
+            if (e.Key == Key.S)
+            {
+                DownKeyPressed = true;
+            }
+            if (e.Key == Key.D)
+            {
+                RightKeyPressed = true;
+            }
+        }
+        private void GameTick(object sender, EventArgs e)
+        {
+            if (UpKeyPressed)
+            {
+                SpeedY += Speed;
+            }
+            if (LeftKeyPressed)
+            {
+                SpeedX -= Speed;
+            }
+            if (DownKeyPressed)
+            {
+                SpeedY -= Speed;
+            }
+            if (RightKeyPressed)
+            {
+                SpeedX += Speed;
+            }
+
+            SpeedX = SpeedX * Friction;
+            SpeedY = SpeedY * Friction;
+
+            Canvas.SetTop(KnightPlayer, Canvas.GetTop(KnightPlayer) - SpeedY);
+            Canvas.SetLeft(KnightPlayer, Canvas.GetLeft(KnightPlayer) + SpeedX);
         }
 
         private void ShowMenu(object sender, KeyEventArgs e)
-        { 
-            if(e.Key == Key.Escape && EscMenuFrame.Content != Menu)
+        {
+            if (e.Key == Key.Escape && EscMenuFrame.Content != Menu)
             {
                 EscMenuFrame.Content = Menu;
+                EscMenuFrame.Height = 860;
+                EscMenuFrame.Width = 1920;
             }
             else if (e.Key == Key.Escape && EscMenuFrame.Content == Menu)
             {
