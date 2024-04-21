12547c19eb57a24662cf2787399774502b8ae6f2
Олександр Пилявський <vt231_poi@student.ztu.edu.ua>
2024-04-21 11:28:57 +0300

    Image для KnightPlayer

--- MainMenu/Forms/OptionMenuInGame.xaml.cs
+++ MainMenu/Forms/OptionMenuInGame.xaml.cs
@@ -20,40 +20,14 @@ namespace MainMenu
     public partial class OptionMenuInGame : Window
     {
         List<Page> ControlsPages = new List<Page>();
-        List<LinearGradientBrush> linearGradientBrushes = new List<LinearGradientBrush>();
-        List<Window> Forms = new List<Window>();
         GameForm gameForm = new GameForm();
         public OptionMenuInGame()
         {
-            InitializeComponent();
-            InitializeEmptyBrush();
-            InitializeBrush();
-
+            InitializeComponent(); 
             ControlsPages.Add(new Pages.Controls());
             ControlsPages.Add(new Pages.Resolution());
-            OptionControl.Content = ControlsPages[0];
-        }
-        protected void InitializeBrush()
-        {
-            LinearGradientBrush UnderlineBrush = new LinearGradientBrush();
-            UnderlineBrush.StartPoint = new Point(0.5, 0);
-            UnderlineBrush.EndPoint = new Point(0.5, 1);
 
-            UnderlineBrush.GradientStops.Add(new GradientStop(Colors.Black, 0.7));
-            UnderlineBrush.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#FF454545"), 1));
-
-            linearGradientBrushes.Add(UnderlineBrush);
-        }
-        protected void InitializeEmptyBrush()
-        {
-            LinearGradientBrush UnderlineBrush = new LinearGradientBrush();
-            UnderlineBrush.StartPoint = new Point(0.5, 0);
-            UnderlineBrush.EndPoint = new Point(0.5, 1);
-
-            UnderlineBrush.GradientStops.Add(new GradientStop(Colors.Black, 0.7));
-            UnderlineBrush.GradientStops.Add(new GradientStop(Colors.Black, 1));
-
-            linearGradientBrushes.Add(UnderlineBrush);
+            OptionControlInGame.Content = ControlsPages[1];
         }
 
         private void QuitBtnOption_Click(object sender, RoutedEventArgs e)
@@ -62,22 +36,6 @@ namespace MainMenu
             Task.Delay(1000);
             this.Hide();
 
-        }
-
-        private void ShowGameOptions(object sender, RoutedEventArgs e)
-        {
-            OptionControl.Content = ControlsPages[0];
-
-            GameOptionsUnderline.Background = linearGradientBrushes[1];
-            WindowOptionsUnderline.Background = linearGradientBrushes[0];
-        }
-
-        private void ShowWindowOptions(object sender, RoutedEventArgs e)
-        {
-            OptionControl.Content = ControlsPages[1];
-
-            GameOptionsUnderline.Background = linearGradientBrushes[0];
-            WindowOptionsUnderline.Background = linearGradientBrushes[1];
         }
     }
 }
